252a24777c54efbe7b976311b0af763a78a7f652
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2025-03-09 00:54:42 +0500

    Inquire button updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ export const Header = ({ initialState, toggleMenuFunction }) => {
     //   }
     // });
 
-    inquireButtonRef.current.addEventListener("mouseenter", () => {
+    const handleMouseEnter = () => {
       gsap.to(inquireRightArrowRef.current, {
         x: "80%",
         opacity: 0,
@@ -61,74 +61,79 @@ export const Header = ({ initialState, toggleMenuFunction }) => {
         delay: 0.3,
         duration: 0.5,
       });
+    };
 
-      inquireButtonRef.current.addEventListener("mouseleave", () => {
-        gsap.to(inquireRightArrowRef.current, {
-          x: "0%",
-          duration: 0.8,
-          opacity: 1,
-          ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
-        });
-
-        gsap.fromTo(
-          inquireLeftArrowRef,
-          {
-            x: "35%",
-            opacity: "1",
-          },
-          {
-            x: "0%",
-            opacity: "0",
-
-            duration: 0.8,
-            opacity: 0,
-            ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
-          }
-        );
-
-        gsap.to(inquireBoxRef.current, {
-          x: "0%",
-          duration: 0.8,
-          ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
-        });
-
-        gsap.to(".white-plate", {
-          y: "100%",
-          ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
-        });
-        gsap.to(inquireLeftArrowRef.current, {
-          opacity: 0,
-          duration: 0.5,
-        });
-
-        gsap.to("h3, i", {
-          color: "white",
-          delay: 0.2,
-          duration: 0.5,
-        });
+    const handleMouseLeave = () => {
+      gsap.to(inquireRightArrowRef.current, {
+        x: "0%",
+        duration: 0.8,
+        opacity: 1,
+        ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
       });
-    });
-  }, {});
+
+      gsap.fromTo(
+        inquireLeftArrowRef.current,
+        {
+          x: "35%",
+          opacity: 1,
+        },
+        {
+          x: "0%",
+          opacity: 0,
+          duration: 0.8,
+          ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
+        }
+      );
+
+      gsap.to(inquireBoxRef.current, {
+        x: "0%",
+        duration: 0.8,
+        ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
+      });
+
+      gsap.to(".white-plate", {
+        y: "100%",
+        duration: 0.8,
+        ease: CustomEase.create("custom", "0.87, 0, 0.13, 1"),
+      });
+      gsap.to(inquireLeftArrowRef.current, {
+        opacity: 0,
+        duration: 0.5,
+      });
+
+      gsap.to("h3, i", {
+        color: "white",
+        delay: 0.2,
+        duration: 0.5,
+      });
+    };
+
+    inquireButtonRef.current.addEventListener("mouseenter", handleMouseEnter);
+    inquireButtonRef.current.addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      inquireButtonRef.current.removeEventListener("mouseenter", handleMouseEnter);
+      inquireButtonRef.current.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+  
   return (
     <header className=" z-[40] fixed top-0 left-0 flex justify-between  items-center flex-row-reverse md:flex-row py-[8vw] md:py-[1.3rem] w-full px-[1.5rem]">
       <div
         ref={hamMenuLRef}
         onClick={toggleMenuFunction}
-        className={` hamburger-menu cursor-pointer group flex items-center gap-[1rem] 2xl:gap-[1vw]`}
-      >
+        className={` hamburger-menu cursor-pointer group flex items-center gap-[1rem] 2xl:gap-[1vw]`}>
         <div className="hamburger-menu-lines ">
           <div
             ref={hamMenuLine1Ref}
             className={` ham-line origin-[21.5%_30%] duration-500 transition-all ${
               initialState ? "rotate-[20deg] " : "rotate-0"
-            } bg-white h-[2.4px] w-[3.5rem] xl:w-[3vw]`}
-          ></div>
+            } bg-white h-[2.4px] w-[3.5rem] xl:w-[3vw]`}></div>
           <div
             ref={hamMenuLine2Ref}
             className={`ham-line origin-[21.5%_30%] duration-500 transition-all ${
               initialState ? "-rotate-[20deg] " : "rotate-0"
-            } bg-white h-[2.4px] w-[3.5rem] xl:w-[3vw] mt-[0.5rem] 2xl:mt-[0.5vw]`}
-          ></div>
+            } bg-white h-[2.4px] w-[3.5rem] xl:w-[3vw] mt-[0.5rem] 2xl:mt-[0.5vw]`}></div>
         </div>
 
         <button className="hidden md:block h-[1rem] overflow-hidden  cursor-pointer relative 2xl:h-[1vw]  ">
@@ -136,16 +141,14 @@ export const Header = ({ initialState, toggleMenuFunction }) => {
             ref={menuTextRef}
             className={` ${
               initialState ? "-translate-y-[100%]" : "translate-y-0"
-            }  absolute left-0 top-0 duration-300 transition-all  mc-button font-nHaas65 leading-none text-[1rem] 2xl:text-[1vw] text-white`}
-          >
+            }  absolute left-0 top-0 duration-300 transition-all  mc-button font-nHaas65 leading-none text-[1rem] 2xl:text-[1vw] text-white`}>
             MENU
           </h3>
           <h3
             ref={closeTextRef}
             className={` ${
               initialState ? "translate-y-0" : "translate-y-[100%]"
-            }  duration-300 transition-transform left-0 mc-button font-nHaas65 leading-none text-[1rem]  2xl:text-[1vw] text-white`}
-          >
+            }  duration-300 transition-transform left-0 mc-button font-nHaas65 leading-none text-[1rem]  2xl:text-[1vw] text-white`}>
             CLOSE
           </h3>
           {/* <h3 className={`mc-button font-nHaas65 leading-none text-[1rem] group-hover:-translate-y-8 2xl:text-[1vw] text-white`}>
@@ -173,28 +176,24 @@ export const Header = ({ initialState, toggleMenuFunction }) => {
       <Link href="/">
         <div
           ref={inquireButtonRef}
-          className=" hidden md:flex relative group: items-center justify-center gap-[5rem] border-[1px] border-[#fff] rounded-[0.2rem] py-[0.5rem] overflow-hidden"
-        >
+          className=" hidden md:flex relative group: items-center justify-center gap-[5rem] border-[1px] border-[#fff] rounded-[0.2rem] py-[0.5rem] overflow-hidden">
           <div
             ref={inquireBoxRef}
-            className=" inline-flex items-center justify-center inquireBox  z-[40]  gap-[0.8vw]"
-          >
+            className=" inline-flex items-center justify-center inquireBox  z-[40]  gap-[0.8vw]">
             {/* <i
               ref={inquireLeftArrowRef}
               className="fa-solid fa-arrow-right inqurie-left-arrow ri-arrow-right-line text-[1rem] 2xl:text-[1vw]  text-white font-[500] opacity-0"
             ></i> */}
             <i
               ref={inquireLeftArrowRef}
-              className="fa-solid fa-arrow-right leftArrow opacity-0 translate-x-4 inqurie-right-arrow ri-arrow-right-line text-[1rem] 2xl:text-[1vw] text-white font-[500] mr-[1vw]"
-            ></i>
+              className="fa-solid fa-arrow-right leftArrow opacity-0 translate-x-4 inqurie-right-arrow ri-arrow-right-line text-[1rem] 2xl:text-[1vw] text-white font-[500] mr-[1vw]"></i>
             <h3 className="uppercase font-nHaas65  text-white text-[1rem]  2xl:text-[1vw]">
               Inquire
             </h3>
           </div>
           <i
             ref={inquireRightArrowRef}
-            className="fa-solid fa-arrow-right inqurie-right-arrow ri-arrow-right-line text-[1rem] 2xl:text-[1vw] text-white font-[500] mr-[1vw]"
-          ></i>
+            className="fa-solid fa-arrow-right inqurie-right-arrow ri-arrow-right-line text-[1rem] 2xl:text-[1vw] text-white font-[500] mr-[1vw]"></i>
 
           <div className="white-plate z-[30] h-full w-full  absolute top-0 left-0 bg-white translate-y-[100%]"></div>
         </div>
